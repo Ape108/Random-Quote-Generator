e5f1f42c1f26f59c8b560e2f9f468439d243cdbd
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2025-07-12 15:55:27 -0500

    Update main & Add requirements.txt

--- quote.py
+++ quote.py
@@ -2,20 +2,18 @@ import requests
 
 def generate_quote():
     response = requests.get("https://zenquotes.io/api/random")
-    if response.status_code == 200:    
-        data = response.json()
-        quote = data[0]['q']
-        author = data[0]['a']
-
-        return f'{quote} - {author}'
-    
-    else:
+    if response.status_code != 200:    
         return 'Failed to fetch a quote'
     
+    data = response.json()
+    quote = data[0]['q']
+    author = data[0]['a']
 
-# printing generated random quote
+    return f'{quote} - {author}'
 
-print(generate_quote())
 
+if __name__ == "__main__":
+    quote = generate_quote()
+    print(quote)
 
 
